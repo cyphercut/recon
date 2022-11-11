0b62c7cc073792f60c0c1a2538b7f9ea0200b340
cris <cristianodpp@gmail.com>
2022-11-11 01:46:55 +0000

    update subdomain replace

--- main.sh
+++ main.sh
@@ -35,8 +35,7 @@ while IFS= read -r subdomain; do
     ################################################
     # DEFINE SUBDOMAIN FOLDER 
     ################################################
-    subdomain_folder="${subdomain//+([://])/_}"
-    echo $subdomain_folder
+    subdomain_folder=$subdomain | sed 's,://,_,g'
 
     ################################################
     # SUBDOMAIN FOLDER 
